e8e43fc572600d58c3e98283102a8f913c42f7fa
“drakke” <drakke.91@gmail.com>
2019-07-23 11:11:54 +0300

    random start snake

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 var snake = document.getElementById("snake");
+var rowPosition = Math.floor(Math.random() * 10) + 1;
+var colPosition = Math.floor(Math.random() * 10) + 1;
+
+console.log("row", rowPosition);
+console.log("col", colPosition);
 
 for(var i = 0; i < 12; i++){
     var div = document.createElement("div");
 
     for(var j = 0; j < 12; j++){
         var createSpan = document.createElement("sapn");
+
         if(i == 0 || i == 11) {
             createSpan.innerText = "#";    
         } else {
             if(j == 0 || j == 11){
                 createSpan.innerText = "#";    
             } else {
+                if(i == rowPosition && j == colPosition) {
+                    createSpan.style.color = "red";    
+                }
                 createSpan.innerText = j;
             }
         }
@@ -20,3 +29,5 @@ for(var i = 0; i < 12; i++){
     
     snake.appendChild(div);
 }
+
+
