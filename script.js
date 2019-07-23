bbcddc92eecee7ba0e6977ea8d1c6159c2f5e423
“drakke” <drakke.91@gmail.com>
2019-07-23 12:11:13 +0300

    add key press right

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 var snake = document.getElementById("snake");
-var rowPosition = Math.floor(Math.random() * 10) + 1;
-var colPosition = Math.floor(Math.random() * 10) + 1;
+var conf = {
+    snake: {
+        rowPosition: positionPoint(),
+        colPosition: positionPoint(),
+    },
+    point: {
+        rowPosition: positionPoint(),
+        colPosition: positionPoint(),
+    }, 
+    sharp: "#"
+}
 
-console.log("row", rowPosition);
-console.log("col", colPosition);
+function positionPoint() {
+    return  Math.floor(Math.random() * 10) + 1;
+}
 
 for(var i = 0; i < 12; i++){
     var div = document.createElement("div");
@@ -12,16 +22,20 @@ for(var i = 0; i < 12; i++){
         var createSpan = document.createElement("sapn");
 
         if(i == 0 || i == 11) {
-            createSpan.innerText = "#";    
+            createSpan.innerText = conf.sharp;    
         } else {
             if(j == 0 || j == 11){
-                createSpan.innerText = "#";    
+                createSpan.innerText = conf.sharp;    
             } else {
-                if(i == rowPosition && j == colPosition) {
-                    createSpan.style.color = "red";    
-                }
+                if(i == conf.snake.colPosition && j == conf.snake.rowPosition) {
+                    createSpan.style.background = "red";    
+                } 
+                if(i == conf.point.colPosition && j == conf.point.rowPosition) {
+                    createSpan.style.background = "#4caf50";
+                }    
                 createSpan.innerText = j;
             }
+            
         }
 
         div.appendChild(createSpan);
@@ -30,4 +44,20 @@ for(var i = 0; i < 12; i++){
     snake.appendChild(div);
 }
 
+addEventListener("keyup", function(e) {
+    
+    if(e.keyCode == 39)  {
+        e.preventDefault();
+        for()
+        while(true) {
+            console.log('position', conf.snake.rowPosition);
+
+            conf.snake.rowPosition += 1; 
+
+            if(conf.snake.rowPosition > 10) {
+                break;
+            }
+        }
+    }
+});
 
