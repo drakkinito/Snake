ca40c46c8ea22b944adbf993643782e0f1cd4028
“drakke” <drakke.91@gmail.com>
2019-07-24 18:04:40 +0300

    fix rigth click

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var snake = document.getElementById("snake");
 var conf = {
     snake: {
-        rowPosition: positionPoint(),
-        colPosition: positionPoint(),
+        rowPosition: 5,
+        colPosition: 5,
     },
     point: {
         rowPosition: positionPoint(),
@@ -19,7 +19,7 @@ for(var i = 0; i < 12; i++){
     var div = document.createElement("div");
 
     for(var j = 0; j < 12; j++){
-        var createSpan = document.createElement("sapn");
+        var createSpan = document.createElement("span");
 
         if(i == 0 || i == 11) {
             createSpan.innerText = conf.sharp;    
@@ -33,7 +33,7 @@ for(var i = 0; i < 12; i++){
                 if(i == conf.point.colPosition && j == conf.point.rowPosition) {
                     createSpan.style.background = "#4caf50";
                 }    
-                createSpan.innerText = j;
+                createSpan.innerText = '0';
             }
             
         }
@@ -45,19 +45,38 @@ for(var i = 0; i < 12; i++){
 }
 
 addEventListener("keyup", function(e) {
+    var coutnSnake = 1;
+    var span = document.getElementsByName('span');
     
+
     if(e.keyCode == 39)  {
         e.preventDefault();
-        for()
-        while(true) {
-            console.log('position', conf.snake.rowPosition);
 
-            conf.snake.rowPosition += 1; 
+        var moveRight = conf.snake.rowPosition += 1;
+        
+        var i = 0;
+        (function() {
+            if (moveRight <= 10) {
+                snake.childNodes[5].childNodes[moveRight].style.background = 'red'
+                moveRight++;
+                setTimeout(arguments.callee, 500);
+            } else {
+                alert('Закончили');
+            } 
+        })();
 
-            if(conf.snake.rowPosition > 10) {
-                break;
-            }
-        }
+        // while(true) {
+            
+        //     var col =  conf.snake.colPosition;
+        //     console.log('---', moveRight);
+        //     this.setInterval(function(){
+        //     }, 1000);
+        //     snake.childNodes[5].childNodes[moveRight].style.background = 'red'
+            
+        //     if(conf.snake.rowPosition > 9) {
+        //         break;
+        //     }
+        // }
     }
 });
 
